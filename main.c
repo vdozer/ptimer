2ca8fa1cbc7170b0e2e6f963906eeb25ce649736
Valentin <vdozer@tuta.com>
2025-09-02 12:14:33 +0300

    Add 1 minute alert
    Give an alert when there's one minute left of working or resting.

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-int main(void) {
+int main() {
 
   int work = 25 * 60;
   int rest = 5 * 60;
@@ -32,17 +32,24 @@ int main(void) {
     for (int x = 0; x < repeat; x++) {
     printf(ANSI_COLOR_RED"Start working now..."ANSI_COLOR_RESET"\n");
     system("paplay /usr/share/sounds/freedesktop/stereo/complete.oga 2>/dev/null");
-    sleep(work);
+    sleep(work - 60);
+    printf(ANSI_COLOR_BLUE"1 minute left!"ANSI_COLOR_RESET"\n");
+    system("paplay /usr/share/sounds/freedesktop/stereo/complete.oga 2>/dev/null");
+    sleep(60);
     for (int y = 0; y < 3; y++) {
     system("paplay /usr/share/sounds/freedesktop/stereo/complete.oga 2>/dev/null"); // This works on my machine but it may not work on others, might change.
     }
     printf(ANSI_COLOR_GREEN"Start break now..."ANSI_COLOR_RESET"\n");
-    sleep(rest);
+    sleep(rest - 60);
+    printf(ANSI_COLOR_BLUE"1 minute left!"ANSI_COLOR_RESET"\n");
+    system("paplay /usr/share/sounds/freedesktop/stereo/complete.oga 2>/dev/null");
+    sleep(60);
     for (int y = 0; y < 3; y++) {
     system("paplay /usr/share/sounds/freedesktop/stereo/complete.oga 2>/dev/null");
     }
     }
     completed(repeat); // Record how many repeats the user has completed.
+    return 0;
   }
   
   if (input == 'n') {
